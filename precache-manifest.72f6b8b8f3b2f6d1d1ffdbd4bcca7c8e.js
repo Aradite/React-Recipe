35d3147341a63a945e40fbda844bf835413a5071
Aradite <58269902+Aradite@users.noreply.github.com>
2020-07-17 18:43:00 -0500

    Updates

---
+++ precache-manifest.72f6b8b8f3b2f6d1d1ffdbd4bcca7c8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a742a49a133f19a7ce21c9408dafd9",
+    "url": "/React-Recipe/index.html"
+  },
+  {
+    "revision": "fb284772036757b73425",
+    "url": "/React-Recipe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "02209b5d00f0dbf24b50",
+    "url": "/React-Recipe/static/css/main.070336e7.chunk.css"
+  },
+  {
+    "revision": "fb284772036757b73425",
+    "url": "/React-Recipe/static/js/2.aaa23e49.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Recipe/static/js/2.aaa23e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02209b5d00f0dbf24b50",
+    "url": "/React-Recipe/static/js/main.89822729.chunk.js"
+  },
+  {
+    "revision": "24812cfa800635b44ebd",
+    "url": "/React-Recipe/static/js/runtime-main.2ffe09e8.js"
+  }
+]);
